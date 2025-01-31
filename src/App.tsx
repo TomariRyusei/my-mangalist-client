d91c2e7f656528d7f8d8965035dca0d88529edf6
TomariRyusei <t.ryu6635@gmail.com>
2025-01-31 22:40:41 +0900

    TanStack Queryでの一覧取得

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,29 @@
-import { useEffect, useState } from "react";
-import { Button } from "@/components/ui/button";
+import { useQuery } from "@tanstack/react-query";
+
+interface manga {
+  id: number;
+  title: string;
+}
+
+const fetchMangaList = async () => {
+  const res = await fetch(`${import.meta.env.VITE_API_URL}/api/mangas`);
+  return res.json();
+};
 
 function App() {
-  const [data, setData] = useState(null);
-
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_API_URL}/api/data`)
-      .then((res) => res.json())
-      .then((data) => setData(data.message))
-      .catch((err) => console.error("Error fetching API:", err));
-  }, []);
+  const { data: mangaList } = useQuery<manga[]>({
+    queryKey: ["mangaList"],
+    queryFn: fetchMangaList,
+  });
 
   return (
-    <div className="bg-zinc-950 dark:bg-white w-full h-screen">
-      <h1 className="text-3xl font-bold underline text-white">Cloudflare Pages + Workers + Hono</h1>
-      <p className="text-white">{data ? data : "Loading..."}</p>
-      <Button variant="outline">Button</Button>
+    <div className="bg-zinc-950 dark:bg-white w-full h-screen p-10 flex flex-col items-center text-white">
+      <h1 className="text-3xl font-bold mb-10">My Manga List</h1>
+      <ul className="">
+        {mangaList?.map((manga) => (
+          <li key={manga.id}>{manga.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
