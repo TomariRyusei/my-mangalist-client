d8ede97fe4e2c893301d494c434274e691baf5be
TomariRyusei <t.ryu6635@gmail.com>
2025-01-31 23:23:38 +0900

    一蘭取得時のローディングとエラー表示

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,27 @@ const fetchMangaList = async () => {
 };
 
 function App() {
-  const { data: mangaList } = useQuery<manga[]>({
+  const {
+    data: mangaList,
+    isPending,
+    isError,
+    error,
+  } = useQuery<manga[]>({
     queryKey: ["mangaList"],
     queryFn: fetchMangaList,
   });
 
+  if (isPending) {
+    return <div className="bg-zinc-950 dark:bg-white h-screen flex justify-center text-white">Loading...</div>;
+  }
+
+  if (isError) {
+    console.error(error);
+    return <div className="bg-zinc-950 dark:bg-white h-screen flex justify-center  text-white">Error occurred</div>;
+  }
+
   return (
-    <div className="bg-zinc-950 dark:bg-white w-full h-screen p-10 flex flex-col items-center text-white">
+    <div className="bg-zinc-950 dark:bg-white h-screen p-10 flex flex-col items-center text-white">
       <h1 className="text-3xl font-bold mb-10">My Manga List</h1>
       <ul className="">
         {mangaList?.map((manga) => (
