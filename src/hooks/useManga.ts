6f25b6b2d03a0108f88697ce33254861fad5b73b
TomariRyusei <t.ryu6635@gmail.com>
2025-02-15 10:48:55 +0900

    APIキーによる認証を削除

--- src/hooks/useManga.ts
+++ src/hooks/useManga.ts
@@ -2,12 +2,10 @@ import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import type { Manga } from "../types/manga";
 
 const API_URL = import.meta.env.VITE_API_URL;
-const API_KEY = import.meta.env.VITE_API_KEY;
 
 const fetchMangaList = async (): Promise<Manga[]> => {
   const res = await fetch(`${API_URL}/mangas`, {
     headers: {
-      "X-API-KEY": API_KEY,
       "Content-Type": "application/json",
     },
   });
@@ -29,7 +27,6 @@ export const useAddManga = () => {
       const res = await fetch(`${API_URL}/mangas`, {
         method: "POST",
         headers: {
-          "X-API-KEY": API_KEY,
           "Content-Type": "application/json",
         },
         body: JSON.stringify({ title }),
@@ -61,7 +58,6 @@ export const useDeleteManga = () => {
       const res = await fetch(`${API_URL}/mangas/${id}`, {
         method: "DELETE",
         headers: {
-          "X-API-KEY": API_KEY,
           "Content-Type": "application/json",
         },
       });
